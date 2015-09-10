13ce6e154f12e17ef72eb753bf1a74c8952b9e8d
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-09-10 01:52:55 -0700

    fix typos and convert to Set storage

--- trie/wordsconcat.rb
+++ trie/wordsconcat.rb
@@ -1,13 +1,13 @@
 require 'set'
+require 'byebug'
 
 class Trie
-  attr_accessor :word_nodes, :root, :both_nodes
+  attr_accessor :word_nodes, :root, :concatenated_words
 
   def initialize
-    @root = TrieNode.new(trie: self)
+    @root = TrieNode.new(trie: self, word_present: false)
     @word_nodes = {}
-    #@concat_nodes = {}
-    @both_nodes = []
+    @concatenated_words = Set.new
   end
 
   def add_word(word)
@@ -15,7 +15,7 @@ class Trie
   end
 
   def import_words(word_list)
-    word_list.each { |word| add_word(word) }
+    word_list.each { |word| add_word(word) unless word.empty? }
     puts "words imported"
   end
 
@@ -27,26 +27,29 @@ class Trie
     queue = @word_nodes.to_a.sort {|x, y| x[0].length <=> y[0].length }
     until queue.empty?
       prefix, node = queue.shift
-      node.all_prefixes.map do |ancestor_node|
-        rest = prefix[-ancestor_node.prefix.length..-1]
-        if rest.empty? 
-          next
-        elsif valid_word?(rest)
-          @both_nodes << node
-          puts node.prefix
-          break
-        else
-          queue << [rest, node]
-        end
+      ancestors_of(prefix, node, queue)
+    end
+  end
+
+  def ancestors_of(string, node, queue)
+    node.all_prefixes.map do |ancestor_node|
+      rest = string[ancestor_node.prefix.length..-1]
+      if rest.nil? or rest.empty? 
+        next
+      elsif valid_word?(rest)
+        @concatenated_words << node.prefix
+        return node.prefix
+      else
+        queue << [rest, node]
       end
     end
-    @both_nodes.sort! {|x,y| x.prefix.length <=> y.prefix.length }
   end
 
   def output_matches
-    puts @both_nodes[-1]
-    puts @both_nodes[-2]
-    puts @both_nodes.length
+    largest = @concatenated_words.max_by {|x| x.length}
+    puts largest
+    puts (@concatenated_words - [largest]).max_by {|x| x.length}
+    puts @concatenated_words.size
   end
 end
 
@@ -71,41 +74,40 @@ class TrieNode
       throw "Bad Search Path, prefix #{prefix} and string #{string}"
     elsif string.length > prefix.length
       next_char = string[prefix.length]
-      if children[next_char]
-        #nothing
-      elsif extend_tree
+      if !children[next_char]
         children[next_char] = TrieNode.new(
-          prefix: "#{prefix}#{next_char}", parent: self)
-      else
-        return nil
+          prefix: "#{prefix}#{next_char}",
+          parent: self, word_present: false)
       end
-      children[next_char].add_into_tree(string, &block)
+      children[next_char].add_word(string)
     else #string and prefix are the same
-      word_present = true
+      @word_present = true
       @trie.word_nodes[prefix] = self
     end
   end
 
   def all_prefixes
-    ancestors = Set.new()
+    ancestors = []
     head = parent
-    until head.parent.nil?
+    until head.nil?
       ancestors << head if head.word_present
       head = head.parent
     end
     ancestors
   end
 
+  def valid_word?(word)
+    @trie.valid_word?(word)
+  end
+
   def to_s
-    "prefix, [#{children}]"
+    "#{prefix}, #{children.size} children"
   end
 end
 
 File.open("wordsforproblem.txt") do |file|
   trie = Trie.new
   trie.import_words(file.lazy.map(&:strip))
-  #trie.import_words(file.map(&:strip))
-  #trie.generate_concatenations
-  trie.find_concatenations
+  trie.find_concat_words
   trie.output_matches
 end
