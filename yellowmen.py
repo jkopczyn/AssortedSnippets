3b671ba86508c2472fb90788ec9c9378aad1fced
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-12-20 16:04:35 -0500

    terrible yellow men puzzle

--- yellowmen.py
+++ yellowmen.py
@@ -1,31 +1,68 @@
-import pdb
+class LetterNode:
+    def __init__(self, letter):
+        self.letter = letter
+        self.predecessors = set()
+        self.followers = set()
+    def simplify(self):
+        ancestors = self.predecessors.copy()
+        queue = list(ancestors)
+        for letter in queue:
+            if letter in ancestors:
+                continue
+            else:
+                ancestors.add(letter)
+                queue.extend(letter.predecessors)
+        descendants = self.followers.copy()
+        queue = list(descendants)
+        for child in queue:
+            child.predecessors -= ancestors
+            queue.extend(child.followers)
+            descendants.add(child)
+        for parent in ancestors:
+            parent.followers -= descendants
+    def singlePath(self):
+        if len(self.followers) > 1 or len(self.predecessors) > 1:
+            return False
+        elif len(self.predecessors) > 0:
+            return (x for x in self.predecessors).next().singlePath()
+        else:
+            string = ""
+            head = self
+            while True:
+                string += head.letter
+                if len(head.followers) > 1:
+                    return False
+                elif len(head.followers) == 0:
+                    return string
+                else:
+                    head = list(head.followers)[0]
 
 def answer(words):
     letters = set(c for w in words for c in w)
-    #print letters
-    orderings = [nextOrdering(words)]
-    #print orderings
-    listOfWordLists = [words]
-    while len(orderings[-1]) < len(letters) and len(listOfWordLists) > 0:
-        #pdb.set_trace()
-        listOfWordLists = [newList for oldList in 
-            listOfWordLists for newList in splitIndex(oldList) if any(newList)]
-        for wordList in listOfWordLists:
-            ordering = nextOrdering(wordList, orderings[-1])
-            #print wordList, orderings, ordering
-            if len(ordering) <= 1:
-                continue
-            elif set(orderings[-1]) - set(ordering):
-                orderings.extend(sorted([ordering, orderings.pop()], key=len))
-            else:
-                orderings[-1] = ordering
-    #print orderings
-    #pdb.set_trace()
-    while len(orderings) > 1:
-        orderings = compressOrderings(orderings, orderings[-1])
-    return "".join(orderings[-1])
+    letterNodes = {c : LetterNode(c) for c in letters}
+    queueOfWordlists = [words]
+    for wl in queueOfWordlists:
+        addToGraph(wl, letterNodes)
+        queueOfWordlists.extend(suffixWordlists(wl))
+    for node in letterNodes.values():
+        node.simplify()
+    for node in letterNodes.values():
+        order = node.singlePath()
+        if order and len(order) == len(letters):
+            return order
+    return False
 
-def splitIndex(words):
+
+def addToGraph(words, letterNodes):
+    firstCharsSet = set()
+    firstCharsOrder = []
+    for c in (w[0] for w in words if len(w) and w[0] not in firstCharsSet):
+        letterNodes[c].predecessors |= set(letterNodes[d] for d in firstCharsSet)
+        for parent in letterNodes[c].predecessors:
+            parent.followers.add(letterNodes[c])
+        firstCharsSet.add(c)
+
+def suffixWordlists(words):
     listOfOrderings = []
     ordering = []
     firstLetter = False
@@ -40,50 +77,7 @@ def splitIndex(words):
             ordering = [w[1:]]
     if len(ordering) > 0:
         listOfOrderings.append(ordering)
-    listOfOrderings.sort(key=lambda l: -len(l))
     return listOfOrderings
-
-def nextOrdering(words, priorOrder=[]):
-    firstChars = [w[0] for w in words if len(w) >= 1]
-    return findImplicitOrder(firstChars, priorOrder)
-
-def findImplicitOrder(chars, priorOrder=[]):
-    explicitOrder = []
-    newCharsSet = set()
-    newCharsOrder = []
-    priorSet = set(priorOrder)
-    for c in (c for c in chars if c not in newCharsSet):
-        newCharsSet.add(c)
-        newCharsOrder.append(c)
-    if not newCharsSet & priorSet:
-        return newCharsOrder
-    for c in newCharsOrder:
-        try:
-            idx = priorOrder.index(c)
-            before = priorOrder[0:idx]
-        except ValueError:
-            before = []
-        explicitOrder.extend([d for d in before if d in (set(before) - set(explicitOrder))])
-        explicitOrder.append(c)
-    try:
-        after = priorOrder[idx+1:]
-    except NameError: #never pulled from priorOrder, idx undefined
-        after = [] #this means the new order hs no overlap
-    explicitOrder.extend(after)
-    return explicitOrder
-
-def compressOrderings(orderings, newOrdering):
-        compressed = []
-        for ordering in orderings:
-            if not (set(ordering) & set(newOrdering)):
-                compressed.append(ordering)
-            betterOrdering = nextOrdering(ordering, newOrdering)
-            if not (set(newOrdering) - set(betterOrdering)):
-                newOrdering = betterOrdering
-            else:
-                compressed.append(betterOrdering)
-        compressed.append(newOrdering)
-        return compressed
 
 print answer(["adc", "bdc", "bdd", "be", "ccc"])
 print answer(["adc", "bac", "bbd", "bd", "ccc", "ccd"])
