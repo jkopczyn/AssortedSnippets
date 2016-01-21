36297bcf1fa02dccc70adf3738d33a90f50c817e
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-01-20 23:19:16 -0800

    fixing the typos and type errors

--- watership.py
+++ watership.py
@@ -22,112 +22,65 @@ def answer(n):
 #wait, there will never be a warren with more than 
 #one nudge available. that simplifies things
 
-class WarrenState():
-    def __init__(self, active={}, inactive={}, multiplicity=1):
-        #active and inactive store group sizes as keys,
-        #and number of groups present that size as values
-        self.active = collections.Counter(active)
-        self.inactive = collections.Counter(inactive)
-        self.multiplicity = multiplicity
-    def __copy__(self):
-        #technically not a proper shallow copy. technically.
-        return WarrenState(self.active.copy(), self.inactive.copy(), self.multiplicity)
-    def copy(self):
-        return copy.copy(self)
-
-    def __deepcopy__(self,memo):
-        act = copy.deepcopy(self.active, memo)
-        inact = copy.deepcopy(self.inactive, memo)
-        return WarrenState(act, inact, self.multiplicity)
-    def deepcopy(self):
-        return copy.deepcopy(self)
-
-    def __eq__(self, other):
-        return self.active == other.active and self.inactive == other.inactive
-        
-    def __hash__(self): #is 17 just a convenient prime, or was there some other reason?
-        return 17*(self.active.hash()+17*(self.inactive.hash()+17*self.mutiplicity))
-
-    def successors(self):
-        newStates = []
-        self.active += Counter()
-        self.inactive += Counter()
-        for size in self.active.most_common():
-            if size > 1:
-                newState = self.deepcopy()
-                newState.multiplicity *= size-1
-                newState.active[size] -= 1
-                newState.inactive[size] += 1
-                newStates.append(newState)
-            for secondSize in self.active.most_common():
-                if secondSize == size and self.active[size] <= 1:
-                    continue
-                newState = self.deepcopy()
-                newState.multiplicity *= secondSize * self.active[secondSize] * self.active[size]
-                newState.active[size] -= 1
-                newState.active[secondSize] -= 1
-                newState.active[size + secondSize] += 1
-                newStates.append(newState)
-            for secondSize in self.inactive.most_common():
-                newState = self.deepcopy()
-                newState.multiplicity *= secondSize * self.inactive[secondSize] * self.active[size]
-                newState.active[size] -= 1
-                newState.inactive[secondSize] -= 1
-                newState.inactive[size + secondSize] += 1
-                newStates.append(newState)
-        return newStates
-
-    def freeze(self):
-        return ((frozenset(self.active.items()),
-                frozenset(self.inactive.items())), self.multiplicity)
-
-
 #so states should have parent lists, each with a number associated.
 #when everything with X+1 active groups is out of the queue, states with X
 #active groups can be calculated explicitly and X+1 groups wiped
 
 def tentative(n):
     start = StateNode(active={1:n}, copies=1)
-    tiers = collections.defaultdict(list,[(n,start)])
-    queue = collections.deque(start)
+    tiers = collections.defaultdict(list,[])
+    queue = collections.deque([start])
     size = n
-    while queue:
-        head = queue.popLeft()
-        if len(head.active) < size:
+    while len(queue) > 0:
+        head = queue.popleft()
+        print '\n\nhead:', head.active, head.inactive, 'copies:', head.copies
+        print "headSize:", sum(head.active.values()), "size:", size, '\n\n'
+        if sum(head.active.values()) < size:
             calcAndClean(tiers, size)
-            size = len(head.active)
-        if head in tiers[size]:
-            continue
+            print 'size drop from', size, 'to', sum(head.active.values()), head.active
+            size = sum(head.active.values())
         for succ, val in head.successors():
-            extant = next(i for i, s in enumerate(tiers[size-1]) if s.warrens() ==
-                    succ.warrens(), None)
+            extant = next((i for i, s in enumerate(tiers[size-1]) if s.warrens() == succ), None)
+            print 'extant?', extant, succ
             if extant:
-                extant.parents[head] = succ.parents[head]
+                extant.parents[head.warrens()] = val
             else:
-                tiers[size-1].append(succ)
-                queue.append(succ)
+                succNode = StateNode(*succ, parents={head.warrens(): val} )
+                if succNode in tiers[size-1]:
+                    print 'already here', succNode, succ, tiers[size-1]
+                    continue
+                tiers[size-1].append(succNode)
+                queue.append(succNode)
     calcAndClean(tiers, 0)
-    cases = sum([node.copies for node in tiers[0]])
-    total = sum([node.copies * max(node.inactive.keys()) for node in tiers[0]])
+    print "final tiers:", tiers
+    return renderOutput(tiers[0])
+
+def renderOutput(nodes):
+    print "nodes:", nodes
+    cases = sum([node.copies for node in nodes])
+    total = sum([node.copies * max(node.inactive.keys()) for node in nodes])
     divideBy = fractions.gcd(cases, total)
+    print total, cases, divideBy
     return str(total/divideBy)+"/"+str(cases/divideBy)
 
 def calcAndClean(tiers, size):
+    print 'calcAndClean', tiers, size
     for node in tiers[size]:
-       node.copies = sum(parent.copies * multiple for parent, multiple in node.parents())
-    del tiers[size+1]
+        print 'calcNode', node.warrens(), node.parents
+        node.copies = sum(parent.copies * multiple for parent, multiple in node.parents.items())
+    #tiers.pop(size+1, None)
 
 #things I need: parents (hash from Node to multiplicity), successors (list), copies (starts as None)
 #StateNodes need to be hashable; active and inactive should be sufficient for
 #this purpose
 
 class StateNode:
-    def __init__(self, active=(), inactive=(), parents={}, copies=None):
-        self.active = dict(active)
-        self.inactive = dict(inactive)
+    def __init__(self, active=(), inactive=(), copies=None, parents={}):
+        self.active = collections.defaultdict(int, active)
+        self.inactive = collections.defaultdict(int, inactive)
         self.parents = parents
         self.copies = copies
-    
+
     def warrens(self):
         return (tuple(self.active.items()), tuple(self.inactive.items()))
 
@@ -138,4 +91,44 @@ class StateNode:
         return self.warrens() == other.warrens()
 
     def successors(self):
-        raise IHaveNoIdeaWhatImDoingException
+        newStates = []
+        removeZeroes(self.active)
+        removeZeroes(self.inactive)
+        activeSizes = frozenset(self.active.keys())
+        inactiveSizes = frozenset(self.inactive.keys())
+        for size in activeSizes:
+            if size > 1:
+                newActive = copy.deepcopy(self.active)
+                newInactive = copy.deepcopy(self.inactive)
+                newActive[size]   -= 1
+                newInactive[size] += 1
+                StateNode.addPrepped(newStates, newActive, newInactive, size-1)
+            for secondSize in activeSizes:
+                if secondSize == size and self.active[size] <= 1:
+                    continue
+                newActive = copy.deepcopy(self.active)
+                newActive[size] -= 1
+                newActive[secondSize] -= 1
+                newActive[size + secondSize] += 1
+                StateNode.addPrepped(newStates, newActive, self.inactive, 
+                    secondSize * self.active[secondSize] * self.active[size])
+            for secondSize in inactiveSizes:
+                newActive = copy.deepcopy(self.active)
+                newInactive = copy.deepcopy(self.inactive)
+                newActive[size] -= 1
+                newInactive[secondSize] -= 1
+                newInactive[size + secondSize] += 1
+                StateNode.addPrepped(newStates, newActive, newInactive, 
+                    secondSize * self.inactive[secondSize] * self.active[size])
+        return newStates
+
+    @classmethod
+    def addPrepped(cls, stateList, activeHash, inactiveHash, multiplicity):
+        stateList.append(((activeHash.items(), inactiveHash.items()), multiplicity))
+        return
+
+def removeZeroes(dictionary):
+    for key in [k for k in dictionary.keys() if dictionary[k] == 0]:
+        del dictionary[key]
+
+print tentative(2)
